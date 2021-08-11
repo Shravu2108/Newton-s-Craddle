227927f297cfa4b0ea99d231564ed796f106304b
Shravu2108 <74601670+Shravu2108@users.noreply.github.com>
2021-08-11 17:56:48 +0530

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,23 @@
+class Roof {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.x=x;
+      this.y=y;
+
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        fill(128,128,128);
+        rectMode(0, 0, this.width, this.height);
+        pop();
+      
+    }
+  };
